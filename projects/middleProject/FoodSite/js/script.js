fc58fbed8342f5bcc46b314fbf7238c165133a57
yrysNM <yrys.n3m@gmail.com>
2022-03-15 08:04:28 +0600

    finally calculator done

--- projects/middleProject/FoodSite/js/script.js
+++ projects/middleProject/FoodSite/js/script.js
@@ -531,11 +531,43 @@ window.addEventListener("DOMContentLoaded", () => {				// loaded dom first
 
 	//Calculator colorie
 	const result = document.querySelector(".calculating__result span"); 
-	let sex = 'female', height, weight, age, ratio = 1.375; 
+	let sex, height, weight, age, ratio; 
 
+	if(window.localStorage.getItem("sex")) {
+		sex = window.localStorage.getItem("sex");
+	}else {
+		sex= "female"; 
+		window.localStorage.setItem('sex', "female");
+	}
+
+	if(window.localStorage.getItem("ratio")) {
+		sex = window.localStorage.getItem("ratio");
+	}else {
+		ratio = 1.375; 
+		window.localStorage.setItem('ratio', 1.375);
+	}
+
+
+	function initialLocalSettings(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
+
+		elements.forEach(elem => {
+			elem.classList.remove(activeClass);
+			if(elem.getAttribute("id") === window.localStorage.getItem("sex")) {
+				elem.classList.add(activeClass);
+			}
+
+			if(elem.getAttribute('data-ratio') === window.localStorage.getItem("ratio")) {
+				elem.classList.add(activeClass);
+			}
+		});
+	}
+
+	initialLocalSettings("#gender div",  "calculating__choose-item_active");
+	initialLocalSettings(".calculating__choose_big div", "calculating__choose-item_active");
 
 	function calcTotal() {
-		console.log(sex, height, weight , age, ratio);
+		//console.log(sex, height, weight , age, ratio);
 		if(!sex || !height || !weight || !age || !ratio) {
 			result.textContent = "____";
 			return;
@@ -558,8 +590,13 @@ window.addEventListener("DOMContentLoaded", () => {				// loaded dom first
 				elem.addEventListener("click", (e) =>  {
 				if(e.target.getAttribute('data-ratio')) {
 					ratio = +e.target.getAttribute('data-ratio');
+
+					window.localStorage.setItem("ratio", +e.target.getAttribute('data-ratio'));
+
 				}else{
 					sex = e.target.getAttribute("id");
+
+					window.localStorage.setItem("sex", e.target.getAttribute("id"));
 				}
 
 				//console.log(ratio, sex);
@@ -583,6 +620,14 @@ window.addEventListener("DOMContentLoaded", () => {				// loaded dom first
 
 
 		input.addEventListener("input", () => {
+			
+			if(input.value.match(/\D/g)) {
+				input.style.border = "1px solid red";				// any text or maybe check show
+			}else {
+				input.style.border = "none";
+			}
+
+
 			switch(input.getAttribute('id')) {
 				case "height": 
 					height = +input.value;

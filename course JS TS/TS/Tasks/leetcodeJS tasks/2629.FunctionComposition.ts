49bec3bc1c1eb3dfd0698038fac7b2e7e09c97e2
yrysNM <yrys.n3m@gmail.com>
2023-04-14 19:59:02 +0600

    leetcode tasks

---
+++ course JS TS/TS/Tasks/leetcodeJS tasks/2629.FunctionComposition.ts
@@ -0,0 +1,40 @@
+/**
+ * 
+ * Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+Output: 65
+Explanation:
+Evaluating from right to left ...
+Starting with x = 4.
+2 * (4) = 8
+(8) * (8) = 64
+(64) + 1 = 65   
+ */
+
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+  return (x) => {
+    if (functions.length <= 0) {
+      return x;
+    } else {
+      return functions.reduceRight((val, f) => f(val), x);
+    }
+  };
+}
+
+const fn = compose([(x) => x + 1, (x) => 2 * x]);
+console.log(fn(4));

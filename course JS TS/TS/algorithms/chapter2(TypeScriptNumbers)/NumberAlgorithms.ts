ab1bc1a8ee76d3f152848a06f8e5946c71646b02
yrysNM <yrys.n3m@gmail.com>
2023-06-04 13:48:10 +0600

    feat: add chapter2 algorithms for number

--- course JS TS/TS/algorithms/chapter2(TypeScriptNumbers)/NumberAlgorithms.ts
+++ course JS TS/TS/algorithms/chapter2(TypeScriptNumbers)/NumberAlgorithms.ts
@@ -56,3 +56,87 @@ function primeFactorization(n: number) {
 }
 
 console.log(primeFactorization(234234));
+
+/**
+ * @EXERCISES
+ */
+
+/**
+ * 1) Base: 4, Exponent: 3, Modulus: 5
+    4ˆ3 % 5 = 64 % 5 = 4
+    value = (lastValue x base ) % modulus:
+    value = (1 x 4) % 5 = 4 % 5 = 4
+    value = (4 x 4) % 5 = 16 % 5 = 1
+    value = (1 x 4) % 5 = 4 % 5 = 4
+ */
+
+function modularExponentiation(
+  base: number,
+  exponent: number,
+  modulus: number
+): string {
+  if (modulus == 1) return `task 1) ${0}`;
+
+  let value = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    value = (value * base) % modulus;
+  }
+
+  return `task 1) ${value}`; // O(n)
+}
+
+console.log("-----------Exercises-------------");
+console.log(modularExponentiation(4, 2, 5));
+
+/**
+ * 2) print all primies less then n
+ */
+
+function allPrimesLessThenN(n: number): void {
+  for (let i = 0; i < n; i++) {
+    if (isPrime(i)) {
+      console.log(`task 2) ${i}`); //O(nsqrt(n))
+    }
+  }
+}
+
+allPrimesLessThenN(15);
+
+/**
+ * 3) check for a set of prime factors
+ */
+
+function maxDivide(number: number, divisor: number): number {
+  while (number % divisor === 0) {
+    number /= divisor;
+  }
+
+  return number; //
+}
+
+function isUgly(number: number): boolean {
+  number = maxDivide(number, 2);
+  number = maxDivide(number, 3);
+  number = maxDivide(number, 5);
+
+  return number === 1; // O(log2(n))
+}
+
+function arrayNUglyNumbers(n: number): string {
+  let counter = 0,
+    currentNumber = 1,
+    uglyNumbers: number[] = [];
+
+  while (counter != n) {
+    if (isUgly(currentNumber)) {
+      counter++;
+      uglyNumbers.push(currentNumber);
+    }
+    currentNumber++;
+  }
+
+  return `task 3) ${uglyNumbers}`; //O(n(log2(n)))
+}
+
+console.log(arrayNUglyNumbers(10));

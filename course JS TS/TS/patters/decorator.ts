d81a538a4341536c798b411de625ab59e43d0b93
yrysNM <yrys.n3m@gmail.com>
2023-08-28 14:10:25 +0600

    feat: initial pattern in TS

---
+++ course JS TS/TS/patters/decorator.ts
@@ -0,0 +1,74 @@
+abstract class Car {
+  public description: string = "";
+
+  public getDescription(): string {
+    return this.description;
+  }
+
+  //   public set setDescription(description: string) {
+  //     this.description = description;
+  //   }
+
+  public abstract cost(): number;
+}
+
+class ModelS extends Car {
+  public description = "Model S";
+
+  public cost(): number {
+    return 73000;
+  }
+}
+
+class ModelX extends Car {
+  public description = "Model X";
+
+  public cost(): number {
+    return 77000;
+  }
+}
+
+abstract class CarOptions extends Car {
+  //   decoratedCar: Car;
+  public abstract getDescription(): string;
+  public abstract cost(): number;
+}
+
+class EnhancedAutoPilot extends CarOptions {
+  decoratedCar: Car;
+
+  constructor(car: Car) {
+    super();
+    this.decoratedCar = car;
+  }
+
+  public getDescription(): string {
+    return this.decoratedCar.getDescription() + ", Enhanced AutoPilot";
+  }
+  public cost(): number {
+    return this.decoratedCar.cost() + 5000;
+  }
+}
+
+class RearFacingSeat extends CarOptions {
+  decoratedCar: Car;
+
+  constructor(car: Car) {
+    super();
+    this.decoratedCar = car;
+  }
+
+  public getDescription(): string {
+    return this.decoratedCar.getDescription() + ", Rear facing seats";
+  }
+
+  public cost(): number {
+    return this.decoratedCar.cost() + 4000;
+  }
+}
+
+let myTesla = new ModelS();
+myTesla = new RearFacingSeat(myTesla);
+
+console.log(myTesla.cost());
+console.log(myTesla.getDescription());

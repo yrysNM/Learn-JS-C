d641f2664c6dc3c706b1fabfa47fc1fdd57c1705
yrysNM <yrys.n3m@gmail.com>
2024-06-10 21:14:35 +0500

    feat: add sort

--- c++/AlgorithmAndStructData/BubleSort.cpp
+++ c++/AlgorithmAndStructData/BubleSort.cpp
@@ -9,13 +9,17 @@ public:
     int digitals[10];
     cout << "Enter 10 numbes: " << endl;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
       cin >> digitals[i];
     }
 
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < i + 1; j++) {
-        if (digitals[i] > digitals[j]) {
+    for (int i = 0; i < 10; i++)
+    {
+      for (int j = 0; j < i + 1; j++)
+      {
+        if (digitals[i] > digitals[j])
+        {
           int temp = digitals[i];
           digitals[i] = digitals[j];
           digitals[j] = temp;
@@ -24,10 +28,9 @@ public:
     }
     cout << "Array sorted: ";
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
       cout << digitals[i] << " ";
     }
-
-    return 0;
   }
 };

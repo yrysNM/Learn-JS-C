3896480126fee45a3bf51ba29511f03da30cbf6f
yrysNM <yrys.n3m@gmail.com>
2024-07-12 19:07:53 +0500

    feat(BinarySearch, LinkedList): add lessons folder

---
+++ c++/practices/lessons/BinarySearch.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int binarySearch(int arr[], int x) {
+  int right = sizeof(*arr) / sizeof(arr);
+  int left = 0;
+  while (left <= right) {
+    int mid = right + left / 2;
+  }
+  return 0;
+}
+
+int main() {
+  int testArr[] = {2, 7, 3, 11, 20, 25, 27, 50, 51, 60};
+  cout << sizeof(testArr) << " " << sizeof(*testArr);
+  return 0;
+}

90e520f82211c2499822fdeb9a6da5c426c7e411
yrysNM <yrys.n3m@gmail.com>
2025-01-17 19:30:28 +0500

    .

--- python/main.py
+++ python/main.py
@@ -1,14 +1,42 @@
-name = input("Enter your name: ")
+# name = input("Enter your name: ")
 
-print(name)
+# print(name)
 
-print(name.capitalize())
-# print(dir(name))
+# print(name.capitalize())
+# # print(dir(name))
 
-# print(name.upper())
+# # print(name.upper())
 
-a = 10
+# a = 10
 
 
-def my_fn():
-    print()
+def sum_nums(num1, num2):
+    sum = num1 + num2
+    return sum
+    print('test')
+
+
+print(sum_nums(5, 5))
+
+test_str = 'Hello world'
+
+print(test_str.index("world"))
+print(len(test_str))
+
+print(dir(test_str))
+
+print(test_str[1:])
+print(test_str[1:10])
+
+
+one_million = 1_000_000
+print(one_million)
+
+print(pow(one_million, 2))
+
+print(type(one_million))
+
+num_test = input('Enter the number: ')
+num_test = int(num_test)
+
+print(num_test, type(num_test))

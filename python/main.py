230c08ed044f8c39ce02e4931a6ceac79422cb7f
yrysNM <yrys.n3m@gmail.com>
2025-01-23 19:19:07 +0500

    feat: add python code

--- python/main.py
+++ python/main.py
@@ -10,33 +10,56 @@
 # a = 10
 
 
-def sum_nums(num1, num2):
-    sum = num1 + num2
-    return sum
-    print('test')
+# def sum_nums(num1, num2):
+#     sum = num1 + num2
+#     return sum
+#     print('test')
 
 
-print(sum_nums(5, 5))
+# print(sum_nums(5, 5))
 
-test_str = 'Hello world'
+# test_str = 'Hello world'
 
-print(test_str.index("world"))
-print(len(test_str))
+# print(test_str.index("world"))
+# print(len(test_str))
 
-print(dir(test_str))
+# print(dir(test_str))
 
-print(test_str[1:])
-print(test_str[1:10])
+# print(test_str[1:])
+# print(test_str[1:10])
 
 
-one_million = 1_000_000
-print(one_million)
+# one_million = 1_000_000
+# print(one_million)
 
-print(pow(one_million, 2))
+# print(pow(one_million, 2))
 
-print(type(one_million))
+# print(type(one_million))
 
-num_test = input('Enter the number: ')
-num_test = int(num_test)
+# num_test = input('Enter the number: ')
+# num_test = int(num_test)
 
-print(num_test, type(num_test))
+# print(num_test, type(num_test))
+
+
+complex_a = 3 + 5j
+complex_b = 4 + 7j
+
+sum_complex = complex_a + complex_b
+
+print(sum_complex)
+
+mul_complex = complex_b * complex_a
+
+print(mul_complex)
+
+print(type(sum_complex))
+
+print(dir(sum_complex))
+
+print(sum_complex.real)
+
+
+print([1, 2, 4] == [1, 2, 3])
+
+print({"a": 5, "b": 5} > {"a": 5, "b": 4})
